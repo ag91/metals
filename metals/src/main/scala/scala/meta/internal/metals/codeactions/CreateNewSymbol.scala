12ec7f86a0245b74b3a1b5597d466250bcac58ec
Andrea <andrea-dev@hotmail.com>
2024-10-24 00:51:02 +0100

    fix: make action handleCommand with infer method
    also select action on the ScalacDiagnostic.NotAMember

--- metals/src/main/scala/scala/meta/internal/metals/codeactions/CreateNewSymbol.scala
+++ metals/src/main/scala/scala/meta/internal/metals/codeactions/CreateNewSymbol.scala
@@ -1,5 +1,6 @@
 package scala.meta.internal.metals.codeactions
 
+import scala.meta.internal.metals.ScalaVersions
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
@@ -8,11 +9,41 @@ import scala.meta.internal.metals.ScalacDiagnostic
 import scala.meta.internal.metals.ServerCommands
 import scala.meta.internal.metals.codeactions.CodeAction
 import scala.meta.pc.CancelToken
+import scala.meta.internal.metals.BuildTargets
+import scala.meta.internal.metals.clients.language.MetalsLanguageClient
+import scala.meta.internal.metals.Compilers
 
 import org.eclipse.{lsp4j => l}
 
-class CreateNewSymbol() extends CodeAction {
+class CreateNewSymbol(
+    compilers: Compilers,
+    buildTargets: BuildTargets,
+    languageClient: MetalsLanguageClient,
+) extends CodeAction {
   override def kind: String = l.CodeActionKind.QuickFix
+
+  override type CommandData = l.TextDocumentPositionParams
+  override def command: Option[ActionCommand] = Some(
+    ServerCommands.InsertInferredMethod
+  )
+
+  override def handleCommand(
+      textDocumentParams: l.TextDocumentPositionParams,
+      token: CancelToken,
+  )(implicit ec: ExecutionContext): Future[Unit] = {
+    val uri = textDocumentParams.getTextDocument().getUri()
+    for {
+      edits <- compilers.insertInferredMethod(
+        textDocumentParams,
+        token,
+      )
+      if (!edits.isEmpty())
+      workspaceEdit = new l.WorkspaceEdit(Map(uri -> edits).asJava)
+      _ <- languageClient
+        .applyEdit(new l.ApplyWorkspaceEditParams(workspaceEdit))
+        .asScala
+    } yield ()
+  }
 
   override def contribute(
       params: l.CodeActionParams,
@@ -21,6 +52,17 @@ class CreateNewSymbol() extends CodeAction {
 
     lazy val parentUri =
       params.getTextDocument.getUri.toAbsolutePath.parent.toURI
+
+    val uri = params.getTextDocument().getUri()
+    val file = uri.toAbsolutePath
+    lazy val isScala3 =
+      (for {
+        buildId <- buildTargets.inverseSources(file)
+        target <- buildTargets.scalaTarget(buildId)
+        isScala3 = ScalaVersions.isScala3Version(
+          target.scalaInfo.getScalaVersion()
+        )
+      } yield isScala3).getOrElse(false)
 
     def createNewSymbol(
         diagnostic: l.Diagnostic,
@@ -41,19 +83,20 @@ class CreateNewSymbol() extends CodeAction {
         diagnostic: l.Diagnostic,
         name: String,
     ): l.CodeAction = {
-      val codeAction = new l.CodeAction()
-      codeAction.setTitle(CreateNewSymbol.method(name))
-      codeAction.setKind(l.CodeActionKind.QuickFix)
-      codeAction.setDiagnostics(List(diagnostic).asJava)
-      codeAction.setCommand(
+      val command =
         ServerCommands.InsertInferredMethod.toLsp(
           new l.TextDocumentPositionParams(
             params.getTextDocument(),
             params.getRange().getStart(),
           )
         )
+
+      CodeActionBuilder.build(
+        title = CreateNewSymbol.method(name),
+        kind = l.CodeActionKind.QuickFix,
+        command = Some(command),
+        diagnostics = List(diagnostic),
       )
-      codeAction
     }
 
     val codeActions = params
@@ -63,13 +106,16 @@ class CreateNewSymbol() extends CodeAction {
       .groupBy {
         case ScalacDiagnostic.SymbolNotFound(name) if name.nonEmpty =>
           Some(name)
+        case ScalacDiagnostic.NotAMember(name) if name.nonEmpty =>
+          Some(name)
         case _ => None
       }
       .collect {
         case (Some(name), diags)
-            if name.head.isLower && params
-              .getRange()
-              .overlapsWith(diags.head.getRange()) =>
+            if !isScala3 // scala 3 not supported yet
+              && name.head.isLower && params
+                .getRange()
+                .overlapsWith(diags.head.getRange()) =>
           createNewMethod(diags.head, name)
         case (Some(name), diags)
             if params.getRange().overlapsWith(diags.head.getRange()) =>
